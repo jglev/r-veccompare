d97f317d13dcd9c3f3cd58b947ba12bbd9778f79
Jacob Levernier <jlevern@upenn.edu>
2017-08-04 20:44:33 -0400

    Implemented venn diagram drawing in knitted output.

--- R/compare.vectors.and.return.text.analysis.of.overlap.R
+++ R/compare.vectors.and.return.text.analysis.of.overlap.R
@@ -15,7 +15,8 @@
 
 compare.vectors.and.return.text.analysis.of.overlap <- function(
 	named_list_of_vectors_to_compare,
-	degrees_of_comparison_to_include = NULL # By default, all degrees of comparison will be included (e.g., for three vectors, all 1-, 2-, and 3-way comparisons). If you only want to include 2- and 3-way comparisons, for example, you can use 'c(2, 3)' here.
+	degrees_of_comparison_to_include = NULL, # By default, all degrees of comparison will be included (e.g., for three vectors, all 1-, 2-, and 3-way comparisons). If you only want to include 2- and 3-way comparisons, for example, you can use 'c(2, 3)' here.
+	print_immediately = FALSE # Whether to immediately print to the console using cat(). This needs to be true if venn diagrams are to be drawn.
 ){
 	output_markdown <- NULL # We'll fill this in below.
 
@@ -34,7 +35,14 @@ compare.vectors.and.return.text.analysis.of.overlap <- function(
 	}
 
 	for(n_way_comparison in degrees_of_comparisons){
-		output_markdown <- paste(output_markdown, "\n",  "\n# ", n_way_comparison, "-Way Comparisons", sep = "", collapse = "")
+
+		addition_to_output_markdown <- paste("\n",  "\n# ", n_way_comparison, "-Way Comparisons", sep = "", collapse = "")
+
+		if(print_immediately == TRUE){
+			cat(addition_to_output_markdown)
+		} else {
+			output_markdown <- paste(output_markdown, addition_to_output_markdown, sep = "", collapse = "")
+		}
 
 		comparisons_at_this_level_of_combination <- combination_set_operations[
 			sapply(
@@ -51,17 +59,33 @@ compare.vectors.and.return.text.analysis.of.overlap <- function(
 			#
 			# Print the results of the set operations comparing the elements:
 			#
-			output_markdown <- paste(output_markdown, "\n\n",  "## **", print.vector.with.and(list_element[["elements_involved"]]), "**", sep = "", collapse = "")
+			addition_to_output_markdown <- paste("\n\n",  "## **", print.vector.with.and(list_element[["elements_involved"]]), "**", sep = "", collapse = "")
 
-			# If we have a venn diagram to draw, go ahead and draw it:
-			if(!is.null(list_element[["venn_diagram"]])){
-				veccompare::render.venn.diagram(list_element[["venn_diagram"]])
+			if(print_immediately == TRUE){
+				cat(addition_to_output_markdown)
+			} else {
+				output_markdown <- paste(output_markdown, addition_to_output_markdown, sep = "", collapse = "")
 			}
 
-			output_markdown <- paste(output_markdown, "\n",  "- Total number of values (not counting duplicates): ", length(list_element[["union_of_elements"]]), sep = "", collapse = "")
+			# If we have a venn diagram to draw, go ahead and draw it:
+			if(print_immediately == TRUE){
+				if(!is.null(list_element[["venn_diagram"]])){
+					cat("\n\n")
+					veccompare::render.venn.diagram(list_element[["venn_diagram"]])
+					cat("\n\n")
+				}
+			}
+
+			addition_to_output_markdown <- paste("\n",  "- Total number of values (not counting duplicates): ", length(list_element[["union_of_elements"]]), sep = "", collapse = "")
+
+			if(print_immediately == TRUE){
+				cat(addition_to_output_markdown)
+			} else {
+				output_markdown <- paste(output_markdown, addition_to_output_markdown, sep = "", collapse = "")
+			}
 
 			if(length(list_element[["elements_involved"]]) > 1){ # If it's not just the element compared with itself:
-				output_markdown <- paste(output_markdown, "\n",
+				addition_to_output_markdown <- paste("\n",
 					"- Total number of elements that **overlap among ",
 					print.vector.with.and(list_element[["elements_involved"]]), ":** ",
 					length(list_element[["overlap_of_elements"]]),
@@ -73,7 +97,13 @@ compare.vectors.and.return.text.analysis.of.overlap <- function(
 					sep = "", collapse = ""
 				)
 
-				output_markdown <- paste(output_markdown, "\n",
+				if(print_immediately == TRUE){
+					cat(addition_to_output_markdown)
+				} else {
+					output_markdown <- paste(output_markdown, addition_to_output_markdown, sep = "", collapse = "")
+				}
+
+				addition_to_output_markdown <- paste("\n",
 					"\t- Items that **overlap among ",
 					print.vector.with.and(list_element[["elements_involved"]]), ":** *",
 					print.vector.with.and(list_element[["overlap_of_elements"]]),
@@ -81,11 +111,17 @@ compare.vectors.and.return.text.analysis.of.overlap <- function(
 					sep = "", collapse = ""
 				)
 
+				if(print_immediately == TRUE){
+					cat(addition_to_output_markdown)
+				} else {
+					output_markdown <- paste(output_markdown, addition_to_output_markdown, sep = "", collapse = "")
+				}
+
 				for(involved_vector_for_getting_unique_elements in list_element[["elements_involved"]]){
 
 					percent_unique_to_involved_vector <- round(length(list_element[["elements_unique_to_first_element"]][[involved_vector_for_getting_unique_elements]])/length(named_list_of_vectors_to_compare[[involved_vector_for_getting_unique_elements]])*100, 2)
 
-					output_markdown <- paste(output_markdown,
+					addition_to_output_markdown <- paste(
 						"\n\n### Elements Unique to ", involved_vector_for_getting_unique_elements,
 						"\n\nTotal number of elements that are **unique to ",
 						involved_vector_for_getting_unique_elements, ":** ",
@@ -95,7 +131,14 @@ compare.vectors.and.return.text.analysis.of.overlap <- function(
 						" (", percent_unique_to_involved_vector, "% of ", involved_vector_for_getting_unique_elements, "; put differently, ", 100-percent_unique_to_involved_vector , "% of ", involved_vector_for_getting_unique_elements, " is overlapping)",
 						sep = "", collapse = ""
 					)
-					output_markdown <- paste(output_markdown, "\n",
+
+					if(print_immediately == TRUE){
+						cat(addition_to_output_markdown)
+					} else {
+						output_markdown <- paste(output_markdown, addition_to_output_markdown, sep = "", collapse = "")
+					}
+
+					addition_to_output_markdown <- paste("\n",
 						"\n\nItems that are **unique to ",
 						involved_vector_for_getting_unique_elements, ":**",
 						"\n\n>*",
@@ -103,6 +146,12 @@ compare.vectors.and.return.text.analysis.of.overlap <- function(
 						"*",
 						sep = "", collapse = ""
 					)
+
+					if(print_immediately == TRUE){
+						cat(addition_to_output_markdown)
+					} else {
+						output_markdown <- paste(output_markdown, addition_to_output_markdown, sep = "", collapse = "")
+					}
 				} # End of if statement re: unique elements
 			} # End of if statement re: length of elements involved.
 		} # End of for loop over comparisons_at_this_level_of_combination
