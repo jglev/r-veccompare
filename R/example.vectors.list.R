b42a8f98c5de4cb774b47c455b4e795498863597
Jacob Levernier <jlevern@upenn.edu>
2017-08-18 10:51:44 -0400

    Added documentation to dataset using ROxygen2, and deleted vestigial files.

---
+++ R/example.vectors.list.R
@@ -0,0 +1,6 @@
+#' Example Vectors List
+#'
+#' An example dataset containing several named vectors, which can be compared to one another for overlaps, unique elements, etc.
+#'
+#' @format A list of named vectors.
+"example.vectors.list"
